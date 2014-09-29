7941516348528da88153401e7d524d0ae5a37dec
Kyle J. Roux <kyle@level2designs.com>
2014-09-28 23:15:16 -0700

    Update views.py
    added example view

---
+++ flask_xxl/templates/project/+project.name+/views.py
@@ -0,0 +1,11 @@
+from flask.ext.xxl.baseviews import BaseView
+
+'''
+  Example view class
+  
+class ExampleIndexView(BaseView):
+    _template = 'index.html'
+    
+    def get(self):
+        return self.render()
+'''
